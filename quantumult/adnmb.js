b80380e1edd8d87ad18423c8a039f43d65950c35
NobeKanai <qidada4880@gmail.com>
2020-10-01 12:28:49 +0800

    update adnmb

--- quantumult/adnmb.js
+++ quantumult/adnmb.js
@@ -1,7 +1,14 @@
+/*
+^https?://adnmb3.com/Api/thread\?id= url script-response-body https://raw.githubusercontent.com/NobeKanai/scripts/master/quantumult/adnmb.js
+*/
+
 let body = $response.body;
 let obj = JSON.parse(body);
 
 let replys = obj.replys.filter(reply => reply.id != 9999999);
 
 obj.replys = replys;
-$done(body);
+console.log(obj);
+$done({
+    body: JSON.stringify(obj)
+});
