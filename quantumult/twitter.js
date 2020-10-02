36bb4a64483146b06054328c8906cff77b0c9a1f
NobeKanai <qidada4880@gmail.com>
2020-10-02 12:54:34 +0800

    add counter and console

--- quantumult/twitter.js
+++ quantumult/twitter.js
@@ -8,11 +8,18 @@ let tweets = obj.globalObjects.tweets;
 
 let re = new RegExp(".*ads-api\.twitter\.com.*");
 
+// 计数器
+let count = 0;
+
 Object.keys(tweets).forEach(function (key) {
     let tweet = tweets[key];
     if (tweet.source !== undefined && re.test(tweet.source)) {
         delete tweets[key];
+        count += 1;
     }
 })
 
+if (count !== 0) {
+    console.log(`cleared twitter ads: ${count} times.`);
+}
 $done(JSON.stringify(obj));
