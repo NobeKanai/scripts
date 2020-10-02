9265e8f322fda91313b10331573237c693ae4ef5
NobeKanai <qidada4880@gmail.com>
2020-10-02 12:50:54 +0800

    fix bug

--- quantumult/twitter.js
+++ quantumult/twitter.js
@@ -1,4 +1,4 @@
-// https?://api\.twitter\.com/\d/timeline/home\.json
+// ^https?://api\.twitter\.com/\d/timeline/home\.json
 
 if ($request.method.toLowerCase() == 'options')
     $done();
@@ -8,10 +8,10 @@ let tweets = obj.globalObjects.tweets;
 
 let re = new RegExp(".*ads-api\.twitter\.com.*");
 
-Object.keys(tweets.tweets).forEach(function (key) {
-    let tweet = tweets.tweets[key];
+Object.keys(tweets).forEach(function (key) {
+    let tweet = tweets[key];
     if (tweet.source !== undefined && re.test(tweet.source)) {
-        delete tweets.tweets[key];
+        delete tweets[key];
     }
 })
 
