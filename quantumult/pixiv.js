aa9ff8eb250de02759ce257238f79310f8ad0b65
NobeKanai <qidada4880@gmail.com>
2021-03-04 12:14:46 +0800

    remove console log

--- quantumult/pixiv.js
+++ quantumult/pixiv.js
@@ -5,15 +5,12 @@
 
 // if 403 redirect to parent url
 if ($response.statusCode != 403) {
-    console.log("skipped")
     $done({})
 }
 
 // find pic id
 const path = $request.path;
-console.log("path is " + path)
 const fileName = path.split('/').pop()
-console.log("fileName is " + fileName)
 const picId = /[0-9]+/.exec(fileName)[0]
 
 let modifiedHeaders = $response.headers
